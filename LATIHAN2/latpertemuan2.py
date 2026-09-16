d3f926731f2f055937c1c7c07df95cc3cd979b5b
mraflipaqihatur <raflipaqihatur765@gmail.com>
2026-09-16 10:57:53 +0700

    Add files via upload

---
+++ LATIHAN2/latpertemuan2.py
@@ -0,0 +1,15 @@
+# ==========================================
+# PROGRAM PENGENALAN VARIABEL PYTHON
+# ==========================================
+
+# Membuat variabel
+nama = "m rafli paqihatur rizqy"
+umur = 18
+tinggi = 153
+sudah_menikah = "true"
+
+# menampilkan nilai dan tipe variabel
+print("nama            :", nama, "| Tipe :", type(nama))
+print("Umur            :", umur, "| Tipe :", type(umur))
+print("Tinggi          :", tinggi, "| Tipe :", type(tinggi))
+print("sudah menikah   :", sudah_menikah, "| Tipe :", type(sudah_menikah))
